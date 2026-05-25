4b863a152fc7d427fd30c72fc308752827db013b
David Nilsen <davidnilsen@SibosAppleWatch.localdomain>
2026-05-25 09:16:01 +0200

    Fix Stripe API version

--- app/api/stripe/create-checkout/route.ts
+++ app/api/stripe/create-checkout/route.ts
@@ -4,7 +4,7 @@ import { cookies } from 'next/headers'
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-04-10'
+  apiVersion: '2026-04-22.dahlia'
 })
 
 export const dynamic = 'force-dynamic'
