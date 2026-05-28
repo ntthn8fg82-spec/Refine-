a1160de1eb62bbc32f87921f0c310afa22cd6a57
David Nilsen <davidnilsen@Davids-MacBook-Pro.local>
2026-05-28 11:57:03 +0200

    Fix middleware runtime

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,4 @@
+export const runtime = 'nodejs'
 import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 import { cookies } from 'next/headers'
